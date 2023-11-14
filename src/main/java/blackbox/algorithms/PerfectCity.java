baa6da25c5170f70f0d42c1b0eab35160c19fcf4
GONZALEZ <salvador.gonzalez-nolasco@capgemini.com>
2023-11-14 14:26:26 -0600

    Perfect City implementation

--- src/main/java/blackbox/algorithms/PerfectCity.java
+++ src/main/java/blackbox/algorithms/PerfectCity.java
@@ -2,4 +2,27 @@ package blackbox.algorithms;
 
 public class PerfectCity {
 
+	/**
+	  Consider a city where the streets are perfectly laid out to form an infinite
+	  square grid. In this city finding the shortest path between two given points
+	  (an origin and a destination) is much easier than in other more complex
+	  cities. 
+	  Given user's departure and destination coordinates, each of them
+	  located on some street, find the length of the shortest route between them
+	  assuming that cars can only move along the streets. Each street can be
+	  represented as a straight line defined by the x = n or y = n formula, where n
+	  is an integer. 
+	  Example 
+	  For departure = [0.4, 1] and destination = [0.9, 3],
+	  the output should be solution(departure, destination) = 0.6 + 2 + 0.1 = 2.7, which is the answer.
+	 */
+	public static double solution(double[] departure, double[] destination) {
+
+		double distanceX = Math.abs(departure[0] - destination[0]);
+        double distanceY = Math.abs(departure[1] - destination[1]);
+
+        return distanceX + distanceY;
+			
+	}
+
 }
---
+++ src/test/java/blackbox/algorithms/PerfectCityTest.java
@@ -0,0 +1,22 @@
+package blackbox.algorithms;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class PerfectCityTest {
+	
+	@Test
+    void testSolution() {
+        double[] departure = {0.4, 1};
+        double[] destination = {0.9, 3};
+        double expectedOutput = 2.7;
+        double actualOutput = PerfectCity.solution(departure, destination);
+        assertEquals(expectedOutput, actualOutput, 0.01, "The solution function should calculate the correct distance.");
+        
+        departure =  new double[]{0, 0.2};
+        destination =  new double []{7, 0.5};
+        expectedOutput = 7.7;
+        assertEquals(expectedOutput, actualOutput, 0.01, "The solution function should calculate the correct second distance.");
+    }
+}
+
